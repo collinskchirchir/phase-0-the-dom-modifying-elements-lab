a12a3571c64a90cfed3dd2f1a8f1c8d4dc657df1
Collins Chirchir <collinskchirchir@gmail.com>
2022-12-03 10:40:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+// document.body.append(newHeader);
+
+ newHeader.id ='victory';
+ newHeader.textContent = "COLLINS-CHIRCHIR is the champion";
